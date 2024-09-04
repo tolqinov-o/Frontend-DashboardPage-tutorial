b236c6054268b1b09b29bead1dcc2809b3625d9e
T.Oqilbek <tolqinovoqilbek90@gmail.com>
2024-09-04 16:04:08 +0500

    My_Frontend || Dashboard-Page-Portfolio-Projects

---
+++ Projects-portfolio/js/item.js
@@ -0,0 +1,73 @@
+//Items
+export const cards = [
+    {
+        id: 1,
+        img: "./img/Project/project1.jpg",
+        projectName: "Dashboard Page - 1",
+        projectDemo: "https://dashboard-page-tutorial.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-1",
+    },
+    {
+        id: 2,
+        img: "./img/Project/project2.jpg",
+        projectName: "Dashboard Page - 2",
+        projectDemo: "https://dashboard-page-tutorial-1.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-2",
+    },
+    {
+        id: 3,
+        img: "./img/Project/project3.jpg",
+        projectName: "Dashboard Page - 3",
+        projectDemo: "https://dashboard-page-tutorial-2.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-3",
+    },
+    {
+        id: 4,
+        img: "./img/Project/project4.jpg",
+        projectName: "Dashboard Page - 4",
+        projectDemo: "https://dashboard-page-tutorial-3.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-4",
+    },
+    {
+        id: 5,
+        img: "./img/Project/project5.jpg",
+        projectName: "Dashboard Page - 5",
+        projectDemo: "https://dashboard-page-tutorial-4.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-5",
+    },
+    {
+        id: 6,
+        img: "./img/Project/project6.jpg",
+        projectName: "Dashboard Page - 6",
+        projectDemo: "https://dashboard-page-tutorial-5.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-6",
+    },
+    {
+        id: 7,
+        img: "./img/Project/project7.jpg",
+        projectName: "Dashboard Page - 7",
+        projectDemo: "https://dashboard-page-tutorial-6.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-7",
+    },
+    {
+        id: 8,
+        img: "./img/Project/project8.jpg",
+        projectName: "Dashboard Page - 8",
+        projectDemo: "https://dashboard-page-tutorial-7.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-8",
+    },
+    {
+        id: 9,
+        img: "./img/Project/project9.jpg",
+        projectName: "Dashboard Page - 9",
+        projectDemo: "https://dashboard-page-tutorial-8.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-9",
+    },
+    {
+        id: 10,
+        img: "./img/Project/project10.jpg",
+        projectName: "Dashboard Page - 10",
+        projectDemo: "https://dashboard-page-tutorial-9.netlify.app",
+        projectCode: "https://github.com/tolqinov-o/Frontend-DashboardPage-tutorial/tree/main/DashboardPage-10",
+    },
+];
