f989c366fb3e7afc7397a5e4f8db5316fe0abd0c
T.Oqilbek <164888937+tolqinov-o@users.noreply.github.com>
2024-09-07 16:57:01 +0500

    Update index.js

--- Projects-portfolio/js/index.js
+++ Projects-portfolio/js/index.js
@@ -1,8 +1,4 @@
-import { cards } from "./item.js";
-
 const body = document.querySelector("body"),
-    mode = document.getElementById("mode"),
-    row = document.querySelector(".row");
 let loader = document.getElementById("preloader");
 
 // Scroll Reveal function
@@ -12,8 +8,7 @@ ScrollReveal().reveal("section");
 mode.addEventListener("click", () => {
     body.classList.toggle("dark");
 
-    if (body.classList.contains("dark")) {
-        mode.src = "./img/sun.png";
+   
     } else {
         mode.src = "./img/moon.png";
     }
@@ -23,26 +18,3 @@ mode.addEventListener("click", () => {
 window.addEventListener('load', function () {
     loader.style.display = 'none';
 });
-
-// Load items
-window.addEventListener("DOMContentLoaded", function() {
-    displayMenuItems(cards);
-});
-
-// item cards
-function displayMenuItems(menuItems) {
-    let displayMenu = menuItems.map(function(item) {
-        return `
-        <div class="col">
-            <div class="card rounded-3 border-0 bg-transparent">
-                <img role="button" src=${item.img} class="img-fluid rounded-3" alt=${item.projectName}>
-                <div class="card-body d-flex justify-content-between">
-                    <h5 class="card-title">${item.projectName}</h5>
-                    <p class="card-text"><a href=${item.projectCode} target="_blank" class="navbar-brand"><ion-icon name="logo-github" class="fs-4 me-3"></ion-icon></a> <a href=${item.projectDemo} target="_blank" class="navbar-brand"><ion-icon name="open-outline" class="fs-4 "></ion-icon></a></p>
-                </div>
-            </div>
-        </div>`;
-    });
-    displayMenu = displayMenu.join("");
-    row.innerHTML = displayMenu;
-};
